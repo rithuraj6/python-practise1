c8164a82d9516445dfe83ecef9e387d522c4e0ff
rithu <contactrithuraj@gmail.com>
2026-03-06 10:17:55 +0530

    added jenkins file

---
+++ myproject/myproject/app1/test1.py
@@ -0,0 +1,10 @@
+def students_info(name,age,course):
+    print("students name", name)
+    print("students age is ", age)
+    print("students course is " ,course)
+
+
+
+
+
+students_info(name="amith",age=25,course="civil")
